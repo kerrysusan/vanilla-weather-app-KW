8337c37f8b7be8fa89e705ed87fa8b8bbe8fecf1
kerrysusan <139054571+kerrysusan@users.noreply.github.com>
2023-08-02 15:04:43 +0100

    Integrated open weather API to show current temperature, weather description, wind speed and humidity

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "c7656c96607e5bf046bfb0c99196fe2d";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
